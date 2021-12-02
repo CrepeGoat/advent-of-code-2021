06215cba32a1aafc88e31eebb15504dc1888bed0
CrepeGoat <studentoflife96@gmail.com>
2021-12-01 22:32:12 -0500

    solved day1

--- shell.nix
+++ shell.nix
@@ -4,6 +4,7 @@
     # nativeBuildInputs is usually what you want -- tools you need to run
     nativeBuildInputs = [
         pkgs.python39
+        pkgs.libiconv
         pkgs.cargo
         pkgs.clippy
         pkgs.rustfmt
