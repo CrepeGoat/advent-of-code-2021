96f1963c12192de344543e677db1a5a19d5b5178
CrepeGoat <studentoflife96@gmail.com>
2021-12-06 11:45:49 -0500

    tmp

--- src/bin/day4part1.rs
+++ src/bin/day4part1.rs
@@ -1,4 +1,6 @@
+use core::str::FromStr;
 use std::collections::{HashMap, HashSet};
+use std::io::BufRead;
 use std::ops::Index;
 
 pub struct BingoBoard<T, const COL: usize, const SIZE: usize>([T; SIZE]);
@@ -8,7 +10,7 @@ impl<T, const COL: usize, const SIZE: usize> BingoBoard<T, COL, SIZE> {
         &self.0[(i * COL)..((i + 1) * COL)]
     }
 
-    pub fn row_mut(&mut self, i: usize) -> &[T] {
+    pub fn row_mut(&mut self, i: usize) -> &mut [T] {
         &mut self.0[(i * COL)..((i + 1) * COL)]
     }
 
@@ -35,21 +37,60 @@ impl<T, const COL: usize, const SIZE: usize> Index<(usize, usize)> for BingoBoar
     }
 }
 
-fn first_bingo_winner<ITEMS, BOARDS, T, const COL: usize, const SIZE: usize>(
+fn read_bingo_board<'a, I, T, const COL: usize, const SIZE: usize>(
+    mut lines: I,
+) -> Result<BingoBoard<T, COL, SIZE>, &'static str>
+where
+    T: Copy + FromStr,
+    I: Iterator<Item = &'a str>,
+{
+    let mut result: BingoBoard<T, COL, SIZE> = unsafe { std::mem::uninitialized() };
+
+    for i in 0..(COL / SIZE) {
+        let board_row = &mut result.row_mut(i);
+
+        let mut items = lines
+            .next()
+            .ok_or("not enough lines")?
+            .trim()
+            .split_whitespace()
+            .map(|x| T::from_str(x).map_err(|_| "failed to parse bingo board item"));
+
+        for j in 0..COL {
+            let board_cell = &mut board_row[j];
+            let item = items.next().ok_or("not enough lines")??;
+
+            unsafe {
+                std::ptr::write(board_cell as *mut T, item);
+            }
+        }
+
+        if items.next().is_some() {
+            return Err("too many items in line");
+        }
+    }
+
+    if lines.next().is_some() {
+        return Err("too many lines");
+    }
+
+    Ok(result)
+}
+
+fn first_bingo_winner<ITEMS, BOARDS, T, U, const COL: usize, const SIZE: usize>(
     items: ITEMS,
     boards: BOARDS,
-) -> Result<T, &'static str>
+) -> Result<U, &'static str>
 where
-    T: Copy + std::hash::Hash + std::cmp::Eq,
+    T: Copy + std::hash::Hash + std::cmp::Eq + std::ops::Mul<usize, Output = U>,
     ITEMS: Iterator<Item = T>,
     BOARDS: Iterator<Item = BingoBoard<T, COL, SIZE>>,
-    usize: std::ops::Mul<T>,
 {
-    let board_states: Vec<_> = boards
+    let mut board_states: Vec<_> = boards
         .map(|board| {
-            let vals_lookups: HashMap<_, _> = HashMap::new();
-            let rows_lookups: Vec<HashSet<_>> = Vec::new();
-            let cols_lookups: Vec<HashSet<_>> = Vec::new();
+            let mut vals_lookups: HashMap<_, _> = HashMap::new();
+            let mut rows_lookups: Vec<HashSet<_>> = Vec::new();
+            let mut cols_lookups: Vec<HashSet<_>> = Vec::new();
 
             for i in 0..COL {
                 for j in 0..(SIZE / COL) {
@@ -57,38 +98,49 @@ where
                 }
             }
             for i in 0..(SIZE / COL) {
-                rows_lookups.push(board.row(i).iter().collect());
+                rows_lookups.push(board.row(i).iter().copied().collect());
             }
             for i in 0..COL {
-                cols_lookups.push(board.col_iter(i).collect());
+                cols_lookups.push(board.col_iter(i).copied().collect());
             }
 
             (vals_lookups, rows_lookups, cols_lookups)
         })
         .collect();
 
-    items
-        .flat_map(|item| {
-            board_states
-                .iter_mut()
-                .filter_map(|(vals, rows, cols)| {
-                    vals.remove(&item)
-                        .and_then(|coords| Some((vals, rows, cols, coords)))
-                })
-                .map(|(vals, rows, cols, (i, j))| {
-                    rows[j].remove(&item);
-                    if rows.len() == 0 {
-                        return Err(vals.len() * item);
-                    }
-                    cols[i].remove(&item);
-                    if cols.len() == 0 {
-                        return Err(vals.len() * item);
-                    }
+    for item in items {
+        for (vals, rows, cols) in board_states.iter_mut() {
+            if let Some((i, j)) = vals.remove(&item) {
+                rows[j].remove(&item);
+                if rows.is_empty() {
+                    return Ok(item * vals.len());
+                }
+                cols[i].remove(&item);
+                if cols.is_empty() {
+                    return Ok(item * vals.len());
+                }
+            }
+        }
+    }
+    Err("no boards won the game")
+}
 
-                    Ok(())
-                })
-        })
-        .collect::<Result<_, _>>()
-        .transpose()
-        .map_err(|_| "no boards won the game")
+fn read_input<R: BufRead>(
+    reader: R,
+) -> Result<(Vec<usize>, Vec<BingoBoard<usize, 5, 25>>), Box<dyn std::error::Error>> {
+    let mut lines = reader.lines();
+
+    let result1: Vec<usize> = lines
+        .next()
+        .ok_or("no lines in input")?
+        .split(',')
+        .map(usize::from_str)
+        .collect()?;
+
+    let result2 = Vec::new();
+    while lines.next().is_some() {
+        result2.append(read_bingo_board(lines.take(5))?);
+    }
+
+    Ok((result1, result2))
 }
