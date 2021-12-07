a98eccdcd4196069445ca6358e1d0c5ee33d73c6
CrepeGoat <studentoflife96@gmail.com>
2021-12-07 09:08:06 -0500

    tmp

--- src/bin/day4part1.rs
+++ src/bin/day4part1.rs
@@ -1,56 +1,50 @@
 use core::str::FromStr;
 use std::collections::{HashMap, HashSet};
-use std::convert::TryInto;
+
 use std::io::BufRead;
 
-fn try_into_matrix<T, const ROW: usize, const COL: usize>(
-    data: Vec<Vec<T>>,
-) -> Result<[[T; COL]; ROW], Box<dyn std::error::Error>> {
-    data.into_iter()
-        .map(|r| r.try_into())
-        .collect::<Result<Vec<_>, _>>()
-        .or(Err(Box::new("incorrect row length")))?
-        .try_into()
-        .or(Err(Box::new("incorrect col length")))
-}
-
-fn read_input<R: BufRead>(
+fn read_input<R: BufRead, T>(
     reader: R,
-) -> Result<(Vec<usize>, Vec<BingoBoard<usize, 5, 25>>), Box<dyn std::error::Error>> {
+) -> Result<(Vec<T>, Vec<Vec<Vec<T>>>), Box<dyn std::error::Error>>
+where
+    T: FromStr,
+    <T as std::str::FromStr>::Err: std::error::Error,
+{
     let mut lines = reader.lines().peekable();
 
-    let result1: Vec<usize> = lines
+    let result1 = lines
         .next()
         .ok_or("no lines in input")??
         .split(',')
-        .map(usize::from_str)
-        .collect::<Result<_, _>>()?;
+        .map(T::from_str)
+        .collect::<Result<Vec<T>, _>>()?;
 
     let mut result2 = Vec::new();
     while lines.next().is_some() && lines.peek().is_some() {
-        let board_lines: Vec<_> = (&mut lines)
+        let board_lines: Vec<Vec<T>> = (&mut lines)
             .take(5)
             .map(|s| {
-                s?.trim()
+                s.or(Err("bad line read".to_owned()))?
+                    .trim()
                     .split_whitespace()
-                    .map(usize::from_str)
+                    .map(T::from_str)
                     .collect::<Result<Vec<_>, _>>()
             })
-            .collect::<Result<_, _>>()??;
-        result2.push(read_bingo_board(board_lines.into_iter())?);
+            .collect::<Result<Vec<Vec<T>>, _>>()?;
+        result2.push(board_lines);
     }
 
     Ok((result1, result2))
 }
 
-fn first_bingo_winner_score<ITEMS, BOARDS, T, U, const COL: usize, const SIZE: usize>(
+fn first_bingo_winner_score<ITEMS, BOARDS, T, U, const ROW: usize, const COL: usize>(
     items: ITEMS,
     boards: BOARDS,
 ) -> Result<U, &'static str>
 where
     T: Copy + std::hash::Hash + std::cmp::Eq + std::ops::Mul<usize, Output = U>,
     ITEMS: Iterator<Item = T>,
-    BOARDS: Iterator<Item = BingoBoard<T, COL, SIZE>>,
+    BOARDS: Iterator<Item = Vec<Vec<T>>>,
 {
     let mut board_states: Vec<_> = boards
         .map(|board| {
@@ -58,16 +52,16 @@ where
             let mut rows_lookups: Vec<HashSet<_>> = Vec::new();
             let mut cols_lookups: Vec<HashSet<_>> = Vec::new();
 
-            for i in 0..COL {
-                for j in 0..BingoBoard::<T, COL, SIZE>::ROW {
-                    vals_lookups.insert(board[(i, j)], (i, j));
+            for i in 0..ROW {
+                for j in 0..COL {
+                    vals_lookups.insert(board[i][j], (i, j));
                 }
             }
-            for i in 0..BingoBoard::<T, COL, SIZE>::ROW {
-                rows_lookups.push(board.row(i).iter().copied().collect());
+            for i in 0..ROW {
+                rows_lookups.push(board[i].iter().copied().collect());
             }
-            for i in 0..COL {
-                cols_lookups.push(board.col_iter(i).copied().collect());
+            for j in 0..COL {
+                cols_lookups.push(board.iter().map(|row| row[j]).collect());
             }
 
             (vals_lookups, rows_lookups, cols_lookups)
@@ -95,7 +89,7 @@ fn main() {
     println!("Enter input: ");
     let stdin = std::io::stdin();
 
-    let (items, boards) = read_input(stdin.lock()).unwrap();
+    let (items, boards) = read_input::<_, usize>(stdin.lock()).unwrap();
 
     let score = first_bingo_winner_score(items.into_iter(), boards.into_iter());
     println!("score: {:?}", score);
