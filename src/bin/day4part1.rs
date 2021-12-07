37b17a353ed6b705e65a6252212cf0eebd3cc72a
CrepeGoat <studentoflife96@gmail.com>
2021-12-06 22:38:24 -0500

    tmp

--- src/bin/day4part1.rs
+++ src/bin/day4part1.rs
@@ -6,6 +6,8 @@ use std::ops::Index;
 pub struct BingoBoard<T, const COL: usize, const SIZE: usize>([T; SIZE]);
 
 impl<T, const COL: usize, const SIZE: usize> BingoBoard<T, COL, SIZE> {
+    const ROW: usize = COL / SIZE;
+
     pub fn row(&self, i: usize) -> &[T] {
         &self.0[(i * COL)..((i + 1) * COL)]
     }
@@ -42,26 +44,25 @@ fn read_bingo_board<'a, I, T, const COL: usize, const SIZE: usize>(
 ) -> Result<BingoBoard<T, COL, SIZE>, &'static str>
 where
     T: Copy + FromStr,
-    I: Iterator<Item = &'a str>,
+    I: Iterator<Item = String>,
 {
     let mut result: BingoBoard<T, COL, SIZE> = unsafe { std::mem::uninitialized() };
 
-    for i in 0..(COL / SIZE) {
-        let board_row = &mut result.row_mut(i);
+    for i in 0..BingoBoard::<T, COL, SIZE>::ROW {
+        let mut board_row = (&mut result).row_mut(i);
 
-        let mut items = lines
-            .next()
-            .ok_or("not enough lines")?
+        let line = (&mut lines).next().ok_or("not enough lines")?;
+        let mut items = line
             .trim()
             .split_whitespace()
             .map(|x| T::from_str(x).map_err(|_| "failed to parse bingo board item"));
 
         for j in 0..COL {
-            let board_cell = &mut board_row[j];
+            let mut board_cell = (&mut board_row)[j];
             let item = items.next().ok_or("not enough lines")??;
 
             unsafe {
-                std::ptr::write(board_cell as *mut T, item);
+                std::ptr::write(&mut board_cell as *mut T, item);
             }
         }
 
@@ -77,7 +78,28 @@ where
     Ok(result)
 }
 
-fn first_bingo_winner<ITEMS, BOARDS, T, U, const COL: usize, const SIZE: usize>(
+fn read_input<R: BufRead>(
+    reader: R,
+) -> Result<(Vec<usize>, Vec<BingoBoard<usize, 5, 25>>), Box<dyn std::error::Error>> {
+    let mut lines = reader.lines().peekable();
+
+    let result1: Vec<usize> = lines
+        .next()
+        .ok_or("no lines in input")??
+        .split(',')
+        .map(usize::from_str)
+        .collect::<Result<_, _>>()?;
+
+    let mut result2 = Vec::new();
+    while lines.next().is_some() && lines.peek().is_some() {
+        let board_lines: Vec<_> = (&mut lines).take(5).collect::<Result<_, _>>()?;
+        result2.push(read_bingo_board(board_lines.into_iter())?);
+    }
+
+    Ok((result1, result2))
+}
+
+fn first_bingo_winner_score<ITEMS, BOARDS, T, U, const COL: usize, const SIZE: usize>(
     items: ITEMS,
     boards: BOARDS,
 ) -> Result<U, &'static str>
@@ -93,11 +115,11 @@ where
             let mut cols_lookups: Vec<HashSet<_>> = Vec::new();
 
             for i in 0..COL {
-                for j in 0..(SIZE / COL) {
+                for j in 0..BingoBoard::<T, COL, SIZE>::ROW {
                     vals_lookups.insert(board[(i, j)], (i, j));
                 }
             }
-            for i in 0..(SIZE / COL) {
+            for i in 0..BingoBoard::<T, COL, SIZE>::ROW {
                 rows_lookups.push(board.row(i).iter().copied().collect());
             }
             for i in 0..COL {
@@ -125,23 +147,12 @@ where
     Err("no boards won the game")
 }
 
-fn read_input<R: BufRead>(
-    reader: R,
-) -> Result<(Vec<usize>, Vec<BingoBoard<usize, 5, 25>>), Box<dyn std::error::Error>> {
-    let mut lines = reader.lines().peekable();
+fn main() {
+    println!("Enter input: ");
+    let stdin = std::io::stdin();
 
-    let result1: Vec<usize> = lines
-        .next()
-        .ok_or("no lines in input")??
-        .split(',')
-        .map(usize::from_str)
-        .collect()?;
+    let (items, boards) = read_input(stdin.lock()).unwrap();
 
-    let result2 = Vec::new();
-    while lines.next().is_some() && lines.peek().is_some() {
-        let mut board_lines: Vec<_> = lines.take(5).collect()?;
-        result2.append(read_bingo_board(board_lines.into_iter())?);
-    }
-
-    Ok((result1, result2))
+    let score = first_bingo_winner_score(items.into_iter(), boards.into_iter());
+    println!("score: {:?}", score);
 }
