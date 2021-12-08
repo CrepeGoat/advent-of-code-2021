3bf29b4784a00194fbb50c55161d0bbcc80b346b
CrepeGoat <studentoflife96@gmail.com>
2021-12-07 23:58:10 -0500

    solved day4 part2

--- src/bin/day4part1.rs
+++ src/bin/day4part1.rs
@@ -96,6 +96,66 @@ where
     None
 }
 
+fn last_bingo_winner_score<ITEMS, BOARDS, T, U, const ROW: usize, const COL: usize>(
+    items: ITEMS,
+    boards: BOARDS,
+) -> Option<U>
+where
+    T: Copy
+        + std::hash::Hash
+        + std::cmp::Eq
+        + std::default::Default
+        + std::ops::Add<T, Output = T>
+        + std::ops::Mul<T, Output = U>,
+    ITEMS: Iterator<Item = T>,
+    BOARDS: Iterator<Item = Matrix<T>>,
+{
+    let mut board_states: Vec<_> = boards
+        .map(|board| {
+            let mut vals_lookups: HashMap<T, _> = HashMap::new();
+            let mut rows_lookups: Vec<HashSet<T>> = Vec::new();
+            let mut cols_lookups: Vec<HashSet<T>> = Vec::new();
+
+            for (i, row) in board.iter().enumerate().take(ROW) {
+                for (j, cell) in row.iter().enumerate().take(COL) {
+                    vals_lookups.insert(*cell, (i, j));
+                }
+            }
+            for row in board.iter().take(ROW) {
+                rows_lookups.push(row.iter().copied().collect());
+            }
+            for j in 0..COL {
+                cols_lookups.push(board.iter().map(|row| row[j]).collect());
+            }
+
+            (true, (vals_lookups, rows_lookups, cols_lookups))
+        })
+        .collect();
+
+    let mut result = None;
+
+    for item in items {
+        for (state, (vals, rows, cols)) in board_states.iter_mut().filter(|s| s.0) {
+            if let Some((i, j)) = vals.remove(&item) {
+                let row = &mut rows[i];
+                assert!(row.remove(&item));
+                if row.is_empty() {
+                    result = Some(item * vals.keys().fold(Default::default(), |sum, &x| x + sum));
+                    *state = false;
+                }
+
+                let col = &mut cols[j];
+                assert!(col.remove(&item));
+                if col.is_empty() {
+                    result = Some(item * vals.keys().fold(Default::default(), |sum, &x| x + sum));
+                    *state = false;
+                }
+            }
+        }
+    }
+    result
+}
+
 fn main() {
     println!("Enter input: ");
     let stdin = std::io::stdin();
@@ -103,6 +163,6 @@ fn main() {
     let (items, boards) = read_input::<_, usize>(stdin.lock()).unwrap();
     //println!("items: {:?}\nboards: {:?}", items, boards);
 
-    let score = first_bingo_winner_score::<_, _, _, _, 5, 5>(items.into_iter(), boards.into_iter());
+    let score = last_bingo_winner_score::<_, _, _, _, 5, 5>(items.into_iter(), boards.into_iter());
     println!("score: {:?}", score);
 }
