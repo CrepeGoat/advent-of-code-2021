1e0aeac5bb990f27b9b6700b9ca5fef7bf12f18b
CrepeGoat <studentoflife96@gmail.com>
2021-12-14 14:13:48 -0500

    solved day12 part1!

--- src/bin/day12part1.rs
+++ src/bin/day12part1.rs
@@ -1,6 +1,7 @@
+use aoc_lib::utils::parsing_input;
 use core::str::FromStr;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Edge(String, String);
 
 impl FromStr for Edge {
@@ -29,16 +30,16 @@ pub const NODE_START: Node = Node {
 };
 pub const NODE_END: Node = Node {
     id: 1,
-    is_big: true,
+    is_big: false,
 };
 
 type GraphRef<'a> = &'a [Vec<Node>];
 
 impl Node {
-    fn make_builder<'a>() -> impl (FnMut(&'a str) -> Self) {
-        let mut node_id_lookup = std::collections::HashMap::new();
-        node_id_lookup.insert("start", NODE_START);
-        node_id_lookup.insert("end", NODE_END);
+    fn make_builder() -> impl (FnMut(String) -> Self) {
+        let mut node_id_lookup = std::collections::HashMap::<String, Node>::new();
+        node_id_lookup.insert("start".to_string(), NODE_START);
+        node_id_lookup.insert("end".to_string(), NODE_END);
 
         move |s| {
             if let Some(&node) = node_id_lookup.get(&s) {
@@ -48,6 +49,7 @@ impl Node {
                     id: node_id_lookup.len(),
                     is_big: s.to_uppercase() == s,
                 };
+                println!("node mapping: {:?} == {:?}", s, node.id);
                 node_id_lookup.insert(s, node);
 
                 node
@@ -55,26 +57,25 @@ impl Node {
         }
     }
 
-    pub fn build_graph(inputs: &[Edge]) -> Vec<Vec<Self>> {
+    pub fn build_graph<I: Iterator<Item = Edge>>(iter: I) -> Vec<Vec<Self>> {
         let mut builder = Self::make_builder();
 
-        inputs
-            .iter()
-            .fold(vec![Vec::new(), Vec::new()], |mut result, Edge(s1, s2)| {
-                let n1 = builder(s1);
-                if n1.id >= result.len() {
-                    result.push(Vec::new());
-                }
+        iter.fold(vec![Vec::new(), Vec::new()], |mut result, Edge(s1, s2)| {
+            let n1 = builder(s1);
+            if n1.id >= result.len() {
+                result.push(Vec::new());
+            }
 
-                let n2 = builder(s2);
-                if n2.id >= result.len() {
-                    result.push(Vec::new());
-                }
+            let n2 = builder(s2);
+            if n2.id >= result.len() {
+                result.push(Vec::new());
+            }
 
-                result[n1.id].push(n2);
+            result[n1.id].push(n2);
+            result[n2.id].push(n1);
 
-                result
-            })
+            result
+        })
     }
 }
 
@@ -99,26 +100,36 @@ impl<'a> PathStreamIter<'a> {
         s
     }
 
+    fn pop(&mut self) -> Option<(Node, usize)> {
+        let path_head = self.path.pop()?;
+        let last_exit = self
+            .node_exits
+            .pop()
+            .expect("`path` and `node_exits` should be the same length");
+        self.visited.remove(&path_head);
+
+        Some((path_head, last_exit))
+    }
+
+    fn push(&mut self, new_head: Node, last_exit: usize) {
+        self.path.push(new_head);
+        self.node_exits.push(last_exit);
+        self.visited.insert(new_head);
+    }
+
     pub fn next_ref(&mut self) -> Option<&Vec<Node>> {
         loop {
-            let path_head = self.path.pop()?;
-            let last_exit = self
-                .node_exits
-                .pop()
-                .expect("`path` and `node_exits` should be the same length");
+            let (path_head, last_exit) = self.pop()?;
 
             if path_head == NODE_END {
                 continue;
             }
 
-            for (i, exit) in (last_exit + 1..self.graph[path_head.id].len()).enumerate() {
+            for (i, exit) in (last_exit..self.graph[path_head.id].len()).enumerate() {
                 let next_node = &self.graph[path_head.id][exit];
                 if next_node.is_big || !self.visited.contains(next_node) {
-                    self.path.push(path_head);
-                    self.node_exits.push(last_exit + i + 1);
-
-                    self.path.push(*next_node);
-                    self.node_exits.push(0);
+                    self.push(path_head, last_exit + i + 1);
+                    self.push(*next_node, 0);
 
                     if self.path[self.path.len() - 1] == NODE_END {
                         return Some(&self.path);
@@ -142,7 +153,6 @@ pub fn count_paths(graph: GraphRef) -> usize {
 
     let mut result = 0;
     while let Some(path_ref) = path_iterlike.next_ref() {
-        println!("path ref: {:?}", path_ref);
         result += 1;
     }
 
@@ -154,8 +164,8 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_count_paths_eg1() {
-        let cave_edges = [
+    fn test_build_graph_eg1() {
+        let cave_edges = vec![
             Edge("start".to_string(), "A".to_string()),
             Edge("start".to_string(), "b".to_string()),
             Edge("A".to_string(), "c".to_string()),
@@ -164,7 +174,53 @@ mod tests {
             Edge("A".to_string(), "end".to_string()),
             Edge("b".to_string(), "end".to_string()),
         ];
-        let graph = Node::build_graph(&cave_edges);
+        let graph = Node::build_graph(cave_edges.into_iter());
+
+        let nodes = vec![
+            NODE_START,
+            NODE_END,
+            Node {
+                id: 2,
+                is_big: true,
+            },
+            Node {
+                id: 3,
+                is_big: false,
+            },
+            Node {
+                id: 4,
+                is_big: false,
+            },
+            Node {
+                id: 5,
+                is_big: false,
+            },
+        ];
+        assert_eq!(
+            graph,
+            vec![
+                vec![nodes[2], nodes[3]],
+                vec![nodes[2], nodes[3]],
+                vec![nodes[0], nodes[4], nodes[3], nodes[1]],
+                vec![nodes[0], nodes[2], nodes[5], nodes[1]],
+                vec![nodes[2]],
+                vec![nodes[3]],
+            ]
+        );
+    }
+
+    #[test]
+    fn test_count_paths_eg1() {
+        let cave_edges = vec![
+            Edge("start".to_string(), "A".to_string()),
+            Edge("start".to_string(), "b".to_string()),
+            Edge("A".to_string(), "c".to_string()),
+            Edge("A".to_string(), "b".to_string()),
+            Edge("b".to_string(), "d".to_string()),
+            Edge("A".to_string(), "end".to_string()),
+            Edge("b".to_string(), "end".to_string()),
+        ];
+        let graph = Node::build_graph(cave_edges.into_iter());
         println!("graph: {:?}", graph);
 
         let result = count_paths(&graph);
@@ -173,7 +229,7 @@ mod tests {
 
     #[test]
     fn test_count_paths_eg2() {
-        let cave_edges = [
+        let cave_edges = vec![
             Edge("dc".to_string(), "end".to_string()),
             Edge("HN".to_string(), "start".to_string()),
             Edge("start".to_string(), "kj".to_string()),
@@ -185,7 +241,7 @@ mod tests {
             Edge("kj".to_string(), "HN".to_string()),
             Edge("kj".to_string(), "dc".to_string()),
         ];
-        let graph = Node::build_graph(&cave_edges);
+        let graph = Node::build_graph(cave_edges.into_iter());
         println!("graph: {:?}", graph);
 
         let result = count_paths(&graph);
@@ -194,7 +250,7 @@ mod tests {
 
     #[test]
     fn test_count_paths_eg3() {
-        let cave_edges = [
+        let cave_edges = vec![
             Edge("fs".to_string(), "end".to_string()),
             Edge("he".to_string(), "DX".to_string()),
             Edge("fs".to_string(), "he".to_string()),
@@ -214,10 +270,20 @@ mod tests {
             Edge("pj".to_string(), "fs".to_string()),
             Edge("start".to_string(), "RW".to_string()),
         ];
-        let graph = Node::build_graph(&cave_edges);
+        let graph = Node::build_graph(cave_edges.into_iter());
         println!("graph: {:?}", graph);
 
         let result = count_paths(&graph);
         assert_eq!(result, 226);
     }
+}
+
+fn main() {
+    println!("Enter input:");
+    let stdin = std::io::stdin();
+    let parsed_inputs = parsing_input::<_, Edge>(stdin.lock());
+
+    let graph = Node::build_graph(parsed_inputs);
+    let result = count_paths(&graph);
+    println!("paths count: {:?}", result);
 }
