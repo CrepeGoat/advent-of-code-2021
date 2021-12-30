e80a14edc17ba2738843bf67fb2a28ac2ecf5a97
CrepeGoat <studentoflife96@gmail.com>
2021-12-30 10:35:24 -0500

    tmp

--- src/bin/day17.rs
+++ src/bin/day17.rs
@@ -12,25 +12,22 @@ fn arc_pos(vel: i64, t: usize) -> i64 {
     (t * (2 * vel + 1 - t)) / 2
 }
 
-fn arc_1st_xing(vel: i64, target: i64) -> (Option<i64>, Option<i64>) {
+fn arc_xings(vel: i64, target: i64) -> (Option<i64>, Option<i64>) {
     /*
         Calculate the rising index at which the arc will cross a given target value.
     */
     // x(t) = t(2v + 1 - t) / 2
     // => t = (2v + 1 ±√((2v+1)² - 8x)) / 2
     let _2vp1 = 2 * vel + 1;
-    let r_pos = _2vp1 * _2vp1;
-    let r_neg = 8 * target;
 
-    if r_pos < r_neg {
-        return (None, None);
+    if let Some(root) = (_2vp1 * _2vp1 - 8 * target).integer_sqrt_checked() {
+        let fall_idx = (_2vp1 + root >= 0).then(|| (_2vp1 + root + 1) / 2);
+        let rise_idx = fall_idx.and_then(|_| (_2vp1 >= root).then(|| (_2vp1 - root) / 2));
+
+        (rise_idx, fall_idx)
+    } else {
+        (None, None)
     }
-
-    let root = (r_pos - r_neg).integer_sqrt();
-
-    let fall_idx = (_2vp1 + root >= 0).then(|| (_2vp1 + root + 1) / 2);
-    let rise_idx = fall_idx.and_then(|_| (_2vp1 >= root).then(|| (_2vp1 - root) / 2));
-    (rise_idx, fall_idx)
 }
 
 fn target_overlap_x(vel: i64, pos: i64, target: Range<i64>) -> Range<usize> {
